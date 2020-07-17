97026a4c60b2c776751e94237b8448a899a86f31
boolks <bootre@naver.com>
2020-07-17 18:05:50 +0900

    Update file_write_example.py

--- 20200717_python/file_write_example.py
+++ 20200717_python/file_write_example.py
@@ -1,18 +1,29 @@
-
 import os
-from random import random
-from datetime import datetime
-# log 디렉토리가 없으면 log 디렉토리 생성
-if not os.path.isdir('log'):
-    os.mkdir('log')
+import sys
+# python directory_handling.py log/count_log.txt
 
-# file open
-with open('log/count_log.txt', 'a', encoding='utf-8') as f:
-    for i in range(1, 11):
-        # 현재 시간 저장
-        stamp = str(datetime.now())
-        # random 값 저장
-        value = random() * 100000
-        # 현재 시간과 난수를 합해서 txt 파일에 쓰기
-        result = f'시간 : {stamp}, 값 : {value}\n'
-        f.write(result)
+#디렉토리가 없으면 log 디렉토리 생성
+if not os.path.isdir("log"):
+    os.mkdir("log")
+
+if len(sys.argv) >= 2:
+    file_name = sys.argv[1]
+    print(file_name)
+
+    print('file write start')
+    #log/count_log.txt 파일이 존재하지 않으면
+    if not os.path.exists(file_name):
+        f = open(file_name, 'w', encoding="utf8")
+        f.write("기록이 시작됩니다\n")
+        f.close()
+
+    with open(file_name, 'a', encoding="utf8") as f:
+        import random, datetime
+        for i in range(1, 11):
+            stamp = str(datetime.datetime.now())
+            value = random.random() * 1000000
+            log_line = stamp + "\t" + str(value) +" 값이 생성되었습니다\n"
+            f.write(log_line)
+    print('file write ends')
+else:
+    print('enter file name')
