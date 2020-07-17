fb2fae4d100633f1e8a5160c99f734fff64d0952
boolks <bootre@naver.com>
2020-07-17 13:39:15 +0900

    Create module_test.py

---
+++ 20200717_python/module_test.py
@@ -0,0 +1,13 @@
+def say_hello(msg):
+    return 'Hello ' + msg
+
+
+def main():
+    print(say_hello('파이썬'))
+
+
+if __name__ == '__main__':
+    print('직접 실행')
+    main()
+else:
+    print('import 되어 실행됨')
